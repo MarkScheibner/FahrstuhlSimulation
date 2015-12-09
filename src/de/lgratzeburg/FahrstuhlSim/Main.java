12908a6ca933fafbfab1f35d840915748e4027a2
Mark S <mark@scheibner-online.de>
2015-12-09 14:21:23 +0100

    Main-Klasse mit leerer main()-Funktion definiert

---
+++ src/de/lgratzeburg/FahrstuhlSim/Main.java
@@ -0,0 +1,19 @@
+package de.lgratzeburg.FahrstuhlSim;
+
+
+/**
+ * Die Mainklasse definiert den Hauptzugriffspunkt der Applikation und startet das Programm
+ */
+public class Main {
+
+	/**
+	 * Hauptzugriffspunkt der Applikation
+	 * @param args - Befehlszeilen-argumente
+	 */
+	public static void main(String args[]) {
+		// TODO
+		// Das JFrame sollte von hier geöffnet werden
+		// Die Simulation sollte auch von hier gestartet werden, wenn dies nicht in den Bereich des View fällt
+	}
+
+}
