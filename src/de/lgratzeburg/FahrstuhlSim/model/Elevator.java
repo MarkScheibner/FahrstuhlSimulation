bff9e353ef8146b1231a41da4d78473de2a016ba
Lenard <cpljensen@t-online.de>
2015-12-11 23:54:22 +0100

    Die Elevator Klasse verfügt nun über eine changeDoorStatus, welche dem Fahrstuhl die Fähigkeit gibt seine Tür zu öffnen und zu schließen.

--- src/de/lgratzeburg/FahrstuhlSim/model/Elevator.java
+++ src/de/lgratzeburg/FahrstuhlSim/model/Elevator.java
@@ -26,15 +26,28 @@ public class Elevator {
 	//Momentaner Zustand der Tür
 	private DoorState doorState= DoorState.CLOSED;
 
-	// Noch nicht ganz genau beschriebene Eigenschaft, welche vllt unn�tig ist
+	/** Noch nicht ganz genau beschriebene Eigenschaft, welche vllt unn�tig ist
+	 * (Wert lässt sich möglicherweise in einer setDoorSpeed Methode verändern)
+	 */
 	private float doorRuntime=0;
+
+	/**Noch nicht ganz genau bestimmter Wert, um die Tür kurzzeitg in einer halboffenen/geschlossenen Phase zu lassen
+	 * (Wert lässt sich möglicherweise in einer setDoorSpeed Methode verändern)
+	 */
+	private int doorMoveSpeed = 1000;
+
+	// Gwünschter Zustand der Tür
+	boolean open = false;
 	
 	public Elevator(int verticalPos, int schachtID){
 		this.verticalPos=verticalPos;
 		this.schachtID=schachtID;
 	}
 
-	// Methode um die Position mithilfe eines ganzahligen Wertes des Zieles zu ver�ndern
+	/**
+	 *  Methode um die Position mithilfe eines ganzahligen Wertes des Zieles zu veraendern
+	 * @param targetLevel - gewünschter Endwert der vertikalen Fahrstuhlposition
+	 */
 	public void changeVertPos(int targetLevel){
 		this.targetLevel = targetLevel;
 		// bestimmung der Fahrtrichtung
@@ -56,6 +69,15 @@ public class Elevator {
 				this.verticalPos =Math.round(( verticalPos + 0.1)*10d)/10d;
 				System.out.println("Elevator level: "+ this.verticalPos);
 
+				/**
+				 * Simuliert eine "Bewegung" des Fahrstuhls
+				 */
+				try {
+					Thread.sleep(400);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
 				// Debugg um die Pos zu sehen
 				System.out.println("Elevator level: "+ this.verticalPos);
 			}
@@ -64,6 +86,15 @@ public class Elevator {
 				// rundet die Kommazahl
 				this.verticalPos =Math.round(( verticalPos - 0.1)*10d)/10d;
 				System.out.println("Elevator level: "+ this.verticalPos);
+
+				/**
+				 * Simuliert eine "Bewegung" des Fahrstuhls
+				 */
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
 			}
 
@@ -81,8 +112,75 @@ public class Elevator {
 	public int getSchachtID(){
 		return this.schachtID;
 	}
-	
-	 
+
+
+
+	/**
+	 * Methode um die Tür zu öffnen und zu schließen, indem man einen bool Wert für Tür eingibt
+	 * @param open - Wenn true dann soll die Tür sich öffnen
+	 */
+	public void changeDoorStatus(boolean open){
+		this.open = open;
+		if  (this.open && doorState != DoorState.OPENED){
+			this.doorState = DoorState.OPENING;
+
+			/**
+			 * Debugg Code
+			 */
+			System.out.println("Tür wird geöffnet, bitte haben Sie Geduld");
+
+
+			/**
+			 * Lässt die Tür kurzzeitig zwischen offen und geschlossen verweilen
+			 * (Nutzen muss noch diskutiert werden)
+			 */
+			try {
+				Thread.sleep(doorMoveSpeed);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			this.doorState = DoorState.OPENED;
+
+			/**
+			 * Debugg Code
+			 */
+			System.out.println("Tür ist jetzt offen");
+			return;
+		}
+		/**
+		 * Selbes Szenario, bloß mit der Angabe die Tür zu öffnen
+		 */
+		if (!this.open && doorState != DoorState.CLOSED){
+			this.doorState = DoorState.CLOSING;
+
+			/**
+			 * Debugg Code
+			 */
+			System.out.println("Tür wird geschlossen, bitte haben Sie Geduld");
+
+
+			/**
+			 * Lässt die Tür kurzzeitig zwischen offen und geschlossen verweilen
+			 * (Nutzen muss noch diskutiert werden)
+			 */
+			try {
+				Thread.sleep(doorMoveSpeed);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			this.doorState = DoorState.CLOSED;
+
+			/**
+			 * Debugg Code
+			 */
+			System.out.println("Tür ist jetzt geschlossen");
+			return;
+		}
+
+		System.out.println("Die Tür ist bereits in diesem Zustand ( "+this.doorState +" ), es wurden keine Veränderungen vorgenommen");
+	}
 	
 	
 	
