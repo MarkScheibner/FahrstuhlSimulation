db4cafcd2c44617b62f1f18d6d14d85fdba3bbaf
Lenard <cpljensen@t-online.de>
2015-12-11 14:56:37 +0100

    Enum Werte eingesetzt

--- src/de/lgratzeburg/FahrstuhlSim/model/Fahrstuhl.java
+++ src/de/lgratzeburg/FahrstuhlSim/model/Fahrstuhl.java
@@ -10,12 +10,12 @@ public class Fahrstuhl {
 	
 
 	// TODO ID-Werte durch Enum-Werte ersetzen
-
+	//change!
 	// Werte für Position des Fahrstuhls
 	private double verticalPos=0;
 
-	// Status der Bewegung: 0=ruhend; 1=hoch; 2=runter
-	private int movementState=0;
+	// Status der Bewegung:
+	private MovementState movementState = MovementState.RESTING;
 
 	// Ganze Zahl als Ziel des Fahrstuhls
 	private double targetLevel=0;
@@ -23,8 +23,8 @@ public class Fahrstuhl {
 	// ganzer Wert für Zugehörigkeit zum Schacht
 	private int schachtID=0;
 
-	// Ganzer Wert als Status für Tür: 0=zu; 1=geht zu; 2=öffnet; 3=auf
-	private int doorStat=0;
+	//Momentaner Zustand der Tür
+	private DoorState doorState= DoorState.CLOSED;
 
 	// Noch nicht ganz genau beschriebene Eigenschaft, welche vllt unn�tig ist
 	private float doorRuntime=0;
@@ -39,19 +39,19 @@ public class Fahrstuhl {
 		this.targetLevel = targetLevel;
 		// bestimmung der Fahrtrichtung
 		if (this.targetLevel - verticalPos<0){
-			this.movementState = 2;
+			this.movementState = MovementState.DOWN;
 		} if (this.targetLevel - verticalPos>0){
-			this.movementState = 1;
+			this.movementState = MovementState.UP;
 		} if (this.targetLevel - verticalPos == 0){
-			this.movementState = 0;
+			this.movementState = MovementState.RESTING;
 			// sollte das zahl = mit der Pos sein, führt er nicht
 			return;
 		}
 		
 		
-		if (this.movementState != 0){
+		if (this.movementState != MovementState.RESTING){
 			// dauerhaftes erhöhen der Pos, bis Ziel erreicht
-			while(verticalPos != targetLevel && movementState == 1){
+			while(verticalPos != targetLevel && movementState == MovementState.UP){
 				// rundet die Kommazahl
 				this.verticalPos =Math.round(( verticalPos + 0.1)*10d)/10d;
 				System.out.println("Elevator level: "+ this.verticalPos);
@@ -60,14 +60,14 @@ public class Fahrstuhl {
 				System.out.println("Elevator level: "+ this.verticalPos);
 			}
 			// see above
-			while(verticalPos != targetLevel && movementState == 2){
+			while(verticalPos != targetLevel && movementState == MovementState.DOWN){
 				// rundet die Kommazahl
 				this.verticalPos =Math.round(( verticalPos - 0.1)*10d)/10d;
 				System.out.println("Elevator level: "+ this.verticalPos);
 
 			}
 
-			this.movementState = 0;
+			this.movementState = MovementState.RESTING;
 
 		}
 
