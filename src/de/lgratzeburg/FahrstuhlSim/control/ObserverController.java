8686b93240f0f109f5b341bdd955cc92210887c1
Mark S <mark@scheibner-online.de>
2015-12-11 12:25:56 +0100

    Controller-Klassen erstellt

---
+++ src/de/lgratzeburg/FahrstuhlSim/control/ObserverController.java
@@ -0,0 +1,17 @@
+package de.lgratzeburg.FahrstuhlSim.control;
+
+import de.lgratzeburg.FahrstuhlSim.observer.Observer;
+
+import javax.swing.*;
+
+/**
+ * Der ObserverController ist der Teil des Controllers, welcher auf die Eingaben des Views reagiert und
+ * die Daten des Models anpasst, wenn dies nötig ist
+ */
+public class ObserverController implements Observer {
+
+	public void notify(JComponent actor, String msg) {
+
+	}
+
+}
---
+++ src/de/lgratzeburg/FahrstuhlSim/control/SimulationController.java
@@ -0,0 +1,17 @@
+package de.lgratzeburg.FahrstuhlSim.control;
+
+/**
+ * Der SimulationController kontrolliert die Hauptsimulation und berechnet die einzelnen Zustände unabhäng von den
+ * Eingaben des View
+ */
+public class SimulationController {
+
+	public void runSimulation() {
+
+	}
+
+	public void updateSim(float delta) {
+
+	}
+
+}
